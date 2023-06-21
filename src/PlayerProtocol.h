d25b5eb5a082e91d340b828656a18dbb88a0e34d
Isaac Chou <3578676+isaacchou@users.noreply.github.com>
2023-06-21 12:00:25 -0400

    Use boost::beast::websocket for communication

--- src/PlayerProtocol.h
+++ src/PlayerProtocol.h
@@ -9,49 +9,74 @@
 #include <algorithm>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/ip/tcp.hpp>
 #include "Interface/Renderer.h"
 
-using boost::asio::ip::tcp;
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
-void send_data(tcp::socket& socket, const void* data, size_t size) {
-	boost::system::error_code error;
-	boost::asio::write(socket, boost::asio::buffer(data, size), error);
-	if (error) {
-		throw boost::system::system_error(error);
-	}
-}
+class websocket_session {
+	beast::flat_buffer buffer_;
+    websocket::stream<beast::tcp_stream> ws_;
 
-void read_data(tcp::socket& socket, void* data, size_t size) {
-	boost::system::error_code error;
-	boost::asio::read(socket, boost::asio::buffer(data, size),
-					boost::asio::transfer_exactly(size), error);
-	if (error) {
-		throw boost::system::system_error(error);
-	}
-}
+public:
+	websocket_session(tcp::socket&& socket) : ws_(std::move(socket)) {}
+	~websocket_session() {}
 
-template <typename T>
-void send_data(tcp::socket& socket, T val) {
-	boost::array<T, 1> buf;
-	buf[0] = val;
-	boost::system::error_code error;
-	boost::asio::write(socket, boost::asio::buffer(buf), error);
-	if (error) {
-		throw boost::system::system_error(error);
-	}
-}
+	void close() { ws_.close(websocket::close_code::normal); }
 
-template <typename T>
-T read_data(tcp::socket& socket) {
-	boost::array<T, 1> buf;
-	boost::system::error_code error;
-	boost::asio::read(socket, boost::asio::buffer(buf),
-					boost::asio::transfer_exactly(sizeof(T)), error);
-	if (error) {
-		throw boost::system::system_error(error);
+	void send_data(const void* data, size_t size) {
+		boost::system::error_code error;
+		ws_.write(net::buffer(data, size), error);
+		if (error) {
+			throw boost::system::system_error(error);
+		}
 	}
-	return buf[0];
-}
+	
+	void read_data(void* data, size_t size) {
+		if (buffer_.size() >= size) {
+			memcpy(data, buffer_.cdata().data(), size);
+			buffer_.consume(size);
+		} else {
+			boost::system::error_code error;	
+			size_t n = ws_.read(buffer_, error);	
+			if (error) {
+				throw boost::system::system_error(error);
+			}
+			read_data(data, size);
+		}		
+	}
+
+	template <typename T>
+	void send(T val) {
+		boost::array<T, 1> buf = {val};
+		send_data(buf.data(), sizeof(T));
+	}
+
+	template <typename T>
+	T read() {
+		boost::array<T, 1> buf;
+		read_data(buf.data(), sizeof(T));
+		return buf[0];
+	}
+
+	void init_handshake(const char* host, const char* port) {
+		std::string host_port = host;
+		host_port += ':';
+		host_port += port;
+		ws_.handshake(host_port, "/");
+		ws_.binary(true);
+	}
+
+	void accept_handshake() {
+		ws_.accept();
+		ws_.binary(true);
+	}
+};
 
 class PlayerProtocol
 {
@@ -70,8 +95,8 @@ protected:
 		get_controller,
 		end
 	};
-	boost::asio::io_context m_io_context;
-	std::vector<tcp::socket> m_sockets;
+	net::io_context m_io_context;
+	std::vector<websocket_session*> m_websockets;
 	Controller m_controller;
 
 	// Player protocol has two flavors, send-all and send and read individually
@@ -87,9 +112,12 @@ protected:
 	
 	virtual ~PlayerProtocol() { 
 		delete[] m_buffer;
+		for (auto& session : m_websockets) {
+			delete session;
+		}
 	}
 
-	void send(const void* data, size_t size) { 
+	void send_all(const void* data, size_t size) { 
 		const unsigned char* p = (const unsigned char*)data;
 		while (size > 0) {
 			if (m_bytes_in_buffer == m_max_buffer_size) flush();
@@ -101,159 +129,149 @@ protected:
 			p += n;
 		}
 	}
-
-	// methods to communicate with a single player
-	// always flush the broadcast (send-all) buffer before individual communications
-	template <typename T> void send(int which, T val) { send(which, (const void*)&val, sizeof(T)); }
-	void send(int which, const void* data, size_t size) {
-		flush();
-		send_data(m_sockets[which], data, size);
-	}
-	void read(int which, void* data, size_t size) { 
-		flush();
-		read_data(m_sockets[which], data, size); 
-	}
 	
+	template <typename T> void send_all(T val) {
+		send_all((const void*)&val, sizeof(T));
+	}
+
 	void flush() {
 		if (m_bytes_in_buffer > 0) {
-			for (auto& socket : m_sockets) {
+			for (auto& session : m_websockets) {
 				// send to all
-				send_data(socket, m_buffer, m_bytes_in_buffer);
+				session->send_data(m_buffer, m_bytes_in_buffer);
 			}
 			m_bytes_in_buffer = 0;
 		}
 	}
-
-	template <typename T> void send(T val) { send((const void*)&val, sizeof(T)); }
-	template <typename T> T read(int which) { T val; read(which, (void*)&val, sizeof(T)); return val; }
 };
 
 class PlayerServer : public PlayerProtocol, public Renderer
 {
 protected:	
-	boost::asio::ip::tcp::endpoint m_endpoint;
+	tcp::endpoint m_endpoint;
 	tcp::acceptor m_acceptor;
 
 	glm::vec3 m_camera_pos, m_camera_target;
 	bool m_camera_follow_player;
 
 public:
-	PlayerServer(int port) : m_endpoint(boost::asio::ip::tcp::v4(), port), m_acceptor(m_io_context, m_endpoint),
+	PlayerServer(int port) : m_endpoint(tcp::v4(), port), m_acceptor(m_io_context, m_endpoint),
 							 m_camera_pos(0.f, 0.f, 0.f), m_camera_target(0.f, 0.f, 1.f), m_camera_follow_player(false) {}
 	virtual ~PlayerServer() {}
 
 	void accept_player() {
 		tcp::socket socket(m_io_context);
 		m_acceptor.accept(socket);
-		socket.set_option(boost::asio::ip::tcp::no_delay(true));
-		m_sockets.push_back(std::move(socket));
+		socket.set_option(tcp::no_delay(true));
 		
-		int player_id = (int)m_sockets.size() - 1;
-		send<int>(player_id, player_id);
+		websocket_session* session = new websocket_session(std::move(socket));
+		session->accept_handshake();
+		m_websockets.push_back(session);
+
+		int player_id = (int)m_websockets.size() - 1;
+		session->send<int>(player_id);
 	}
-	virtual int how_many_controllers() { return (int)m_sockets.size(); }
+	virtual int how_many_controllers() { return (int)m_websockets.size(); }
 	virtual void setup_camera(bool follow, const glm::vec3& eye, const glm::vec3& target) {
 		m_camera_follow_player = follow;
 		m_camera_pos = eye;
 		m_camera_target = target;
 	}
-	virtual void pre_connect() { send<Command>(Command::begin_setup); setup_camera(); }
-	virtual void post_connect() { send<Command>(Command::end_setup); flush(); }
-	virtual void begin_update() { send<Command>(Command::begin_update); }
+	virtual void pre_connect() { send_all<Command>(Command::begin_setup); setup_camera(); }
+	virtual void post_connect() { send_all<Command>(Command::end_setup); flush(); }
+	virtual void begin_update() { send_all<Command>(Command::begin_update); }
 	virtual bool end_update(float elapsed_time) {
-		send<Command>(Command::end_update);
-		send<float>(elapsed_time);
+		send_all<Command>(Command::end_update);
+		send_all<float>(elapsed_time);
 		flush();
 
 		bool ret = true;
-		int n = how_many_controllers();
-		for (int i = 0; i < n; i++) {
-			ret = ret && read<bool>(i);
+		for (auto& session : m_websockets) {
+			ret = ret && session->read<bool>();
 		}
 		return ret;
 	}
 	virtual void setup_camera() {
-		send<Command>(Command::setup_camera);
-		send<bool>(m_camera_follow_player);
-		send<glm::vec3&>(m_camera_pos);
-		send<glm::vec3&>(m_camera_target);
+		send_all<Command>(Command::setup_camera);
+		send_all<bool>(m_camera_follow_player);
+		send_all<glm::vec3&>(m_camera_pos);
+		send_all<glm::vec3&>(m_camera_target);
 	}
 	virtual void add_texture(int id, size_t width, size_t height, unsigned char* data) {
-		send<Command>(Command::add_texture);
-		send<int>(id);
-		send<size_t>(width);
-		send<size_t>(height);
+		send_all<Command>(Command::add_texture);
+		send_all<int>(id);
+		send_all<size_t>(width);
+		send_all<size_t>(height);
 		size_t row_bytes = ((((width * 3) + 3) >> 2) << 2);
-		send(data, row_bytes * height);
+		send_all(data, row_bytes * height);
 	}
 	
 	void send_shape_desc(const ShapeDesc& shape_desc, const glm::mat4& trans) {
-		send<ShapeDesc::Type>(shape_desc.m_type);
-		send((void*)shape_desc.m_param, 4 * sizeof(float));
-		send<unsigned int>(shape_desc.m_default_texture);
-		send<size_t>(shape_desc.m_textures.size());
+		send_all<ShapeDesc::Type>(shape_desc.m_type);
+		send_all((void*)shape_desc.m_param, 4 * sizeof(float));
+		send_all<unsigned int>(shape_desc.m_default_texture);
+		send_all<size_t>(shape_desc.m_textures.size());
 		for (unsigned int t : shape_desc.m_textures) {
-			send<unsigned int>(t);
+			send_all<unsigned int>(t);
 		}
-		send<const glm::mat4&>(trans);
+		send_all<const glm::mat4&>(trans);
 
 		if (shape_desc.m_type == ShapeDesc::Type::Compound) {
 			const CompoundShapeDesc& compound = dynamic_cast<const CompoundShapeDesc&>(shape_desc);
 			const std::vector<ShapeDesc::child_desc>& child_desc = compound.get_child_shape_desc();
-			send<size_t>(child_desc.size());
+			send_all<size_t>(child_desc.size());
 			for (auto& desc : child_desc) {
 				send_shape_desc(*desc.m_desc, desc.m_trans);
 			}
 		}
 	}
 	virtual void add_shape(int id, const ShapeDesc& shape_desc, const glm::mat4& trans) {
-		send<Command>(Command::add_shape);
-		send<int>(id);
+		send_all<Command>(Command::add_shape);
+		send_all<int>(id);
 		send_shape_desc(shape_desc, trans);
 	}
 	virtual void update_shape(int id, const glm::mat4& trans) {
-		send<Command>(Command::update_shape);
-		send<int>(id);
-		send<const glm::mat4&>(trans);
+		send_all<Command>(Command::update_shape);
+		send_all<int>(id);
+		send_all<const glm::mat4&>(trans);
 	}
 	virtual void remove_shape(int id) {
-		send<Command>(Command::remove_shape);
-		send<int>(id);
+		send_all<Command>(Command::remove_shape);
+		send_all<int>(id);
 	}
-	virtual void set_player_transform(int which, const glm::mat4& trans) {
-		send<Command>(Command::set_player_transform);
-		send<int>(which);
-		send<const glm::mat4&>(trans);
+	virtual void set_player_transform(int player_id, const glm::mat4& trans) {
+		send_all<Command>(Command::set_player_transform);
+		send_all<int>(player_id);
+		send_all<const glm::mat4&>(trans);
 	}
-	virtual Controller& get_controller(int which) {
-		
-		send<Command>(which, Command::get_controller);
+	virtual Controller& get_controller(int player_id) {
+		flush();
+		auto& session = m_websockets[player_id];
+		session->send<Command>(Command::get_controller);
 
 		m_controller.m_keyboard.clear();
-		size_t n = read<size_t>(which);
+		size_t n = session->read<size_t>();
 		for (int i = 0; i < n; i++) {
-			m_controller.m_keyboard.insert(read<int>(which));
+			m_controller.m_keyboard.insert(session->read<int>());
 		}
 
 		m_controller.m_mouse.clear();
-		n = read<size_t>(which);
+		n = session->read<size_t>();
 		for (int i = 0; i < n; i++) {
-			m_controller.m_mouse.insert(read<int>(which));
+			m_controller.m_mouse.insert(session->read<int>());
 		}
-		m_controller.m_cursor_cur_pos = read<glm::vec2>(which);
-		m_controller.m_cursor_last_pos = read<glm::vec2>(which);
-		m_controller.m_scroll_pos = read<glm::vec2>(which);
+		m_controller.m_cursor_cur_pos = session->read<glm::vec2>();
+		m_controller.m_cursor_last_pos = session->read<glm::vec2>();
+		m_controller.m_scroll_pos = session->read<glm::vec2>();
 		return m_controller;
 	}
 	void disconnect() {
 		// tell all players to disconnect
-		send<Command>(Command::end);		
-		int n = how_many_controllers();
-		for (int i = 0; i < n; i++) {
-			// wait for all players to acknowledge
-			// before closing the connection
-			read<bool>(i);
-			m_sockets[i].close();
+		send_all<Command>(Command::end);
+		flush();
+		
+		for (auto& session : m_websockets) {
+			session->close();
 		}
 	}
 };
@@ -273,18 +291,21 @@ public:
 	void join(const char* host, const char* port) {
 		tcp::resolver::results_type endpoints = m_resolver.resolve(host, port);
 		tcp::socket socket(m_io_context);
-		boost::asio::connect(socket, endpoints);
-		socket.set_option(boost::asio::ip::tcp::no_delay(true));
-		m_sockets.push_back(std::move(socket));
+		net::connect(socket, endpoints);
+		socket.set_option(tcp::no_delay(true));
+		websocket_session* session = new websocket_session(std::move(socket));
+		session->init_handshake(host, port);
+		m_websockets.push_back(session);
+
 		// server assigns a player id after connection
-		m_player_id = read<int>(0);
+		m_player_id = session->read<int>();
 	}
 
-	ShapeDesc* read_shape_desc(glm::mat4& m) {
+	ShapeDesc* read_shape_desc(websocket_session* session, glm::mat4& m) {
 		
-		ShapeDesc::Type type = read<ShapeDesc::Type>(0);
+		ShapeDesc::Type type = session->read<ShapeDesc::Type>();
 		float param[4];
-		read(0, (void*)param, 4 * sizeof(float));
+		session->read_data((void*)param, 4 * sizeof(float));
 
 		ShapeDesc* desc = NULL;
 		CompoundShapeDesc* compound = NULL;
@@ -312,18 +333,18 @@ public:
 		desc->m_param[1] = param[1];
 		desc->m_param[2] = param[2];
 		desc->m_param[3] = param[3];
-		desc->m_default_texture = read<unsigned int>(0);
-		size_t n = read<size_t>(0);
+		desc->m_default_texture = session->read<unsigned int>();
+		size_t n = session->read<size_t>();
 		for (int i = 0; i < n; i++) {
-			desc->m_textures.push_back(read<unsigned int>(0));
+			desc->m_textures.push_back(session->read<unsigned int>());
 		}
-		m = read<glm::mat4>(0);
+		m = session->read<glm::mat4>();
 
 		if (compound != NULL) {
-			n = read<size_t>(0);
+			n = session->read<size_t>();
 			for (int i = 0; i < n; i++) {
 				glm::mat4 mm;
-				compound->add_child_shape_desc(read_shape_desc(mm), mm);
+				compound->add_child_shape_desc(read_shape_desc(session, mm), mm);
 			}
 		}
 		return desc;
@@ -331,7 +352,8 @@ public:
 
 	bool communicate() {
 		bool cont = true;
-		Command cmd = read<Command>(0);
+		auto& session = m_websockets[0];
+		Command cmd = session->read<Command>();
 		switch (cmd) {
 			case Command::begin_setup:
 				m_renderer.pre_connect();
@@ -344,55 +366,55 @@ public:
 				m_renderer.begin_update();
 			break;
 			case Command::end_update:
-				send<bool>(m_renderer.end_update(read<float>(0)));
+				session->send<bool>(m_renderer.end_update(session->read<float>()));
 				flush();
 			break;
 			case Command::setup_camera:
 			{
-				bool follow = read<bool>(0);
-				glm::vec3 eye = read<glm::vec3>(0);
-				glm::vec3 target = read<glm::vec3>(0);
+				bool follow = session->read<bool>();
+				glm::vec3 eye = session->read<glm::vec3>();
+				glm::vec3 target = session->read<glm::vec3>();
 				m_renderer.setup_camera(follow, eye, target);
 			}
 			break;
 			case Command::add_texture:
 			{
-				int id = read<int>(0);
-				size_t width = read<size_t>(0);
-				size_t height = read<size_t>(0);
+				int id = session->read<int>();
+				size_t width = session->read<size_t>();
+				size_t height = session->read<size_t>();
 				size_t row_bytes = ((((width * 3) + 3) >> 2) << 2);
 				unsigned char* data = new unsigned char[row_bytes * height];
-				read(0, data, row_bytes * height);
+				session->read_data(data, row_bytes * height);
 				m_renderer.add_texture(id, width, height, data);
 				delete[] data;
 			}
 			break;
 			case Command::add_shape:
 			{
-				int id = read<int>(0);
+				int id = session->read<int>();
 				glm::mat4 m;
-				ShapeDesc* desc = read_shape_desc(m);
+				ShapeDesc* desc = read_shape_desc(session, m);
 				m_renderer.add_shape(id, *desc, m);
 				delete desc;
 			}
 			break;
 			case Command::update_shape:
 			{
-				int id = read<int>(0);
-				glm::mat4 m = read<glm::mat4>(0);
+				int id = session->read<int>();
+				glm::mat4 m = session->read<glm::mat4>();
 				m_renderer.update_shape(id, m);
 			}
 			break;
 			case Command::remove_shape:
 			{
-				m_renderer.remove_shape(read<int>(0));
+				m_renderer.remove_shape(session->read<int>());
 			}
 			break;
 			case Command::set_player_transform:
 			{
-				int which = read<int>(0);
-				glm::mat4 m = read<glm::mat4>(0);
-				if (which == m_player_id) {
+				int player_id = session->read<int>();
+				glm::mat4 m = session->read<glm::mat4>();
+				if (player_id == m_player_id) {
 					m_renderer.set_player_transform(0, m);
 				}
 			}
@@ -400,23 +422,21 @@ public:
 			case Command::get_controller:
 			{
 				const Controller& ctlr = m_renderer.get_controller(0);
-				// use the send-all method here to take advantage of the buffering	
-				send<size_t>(ctlr.m_keyboard.size());
-				for (int k : ctlr.m_keyboard) { send<int>(k); }
+				session->send<size_t>(ctlr.m_keyboard.size());
+				for (int k : ctlr.m_keyboard) { session->send<int>(k); }
 				
-				send<size_t>(ctlr.m_mouse.size());
-				for (int b : ctlr.m_mouse) { send<int>(b); }
+				session->send<size_t>(ctlr.m_mouse.size());
+				for (int b : ctlr.m_mouse) { session->send<int>(b); }
 
-				send(ctlr.m_cursor_cur_pos);
-				send(ctlr.m_cursor_last_pos);
-				send(ctlr.m_scroll_pos);
-				flush();
+				session->send(ctlr.m_cursor_cur_pos);
+				session->send(ctlr.m_cursor_last_pos);
+				session->send(ctlr.m_scroll_pos);
 			}
 			break;
 			case Command::end:
-				send<bool>(true);
-				flush();
-				m_sockets[0].close();
+				for (auto& session : m_websockets) {
+					session->close();
+				}
 				cont = false;
 			break;
 		}
